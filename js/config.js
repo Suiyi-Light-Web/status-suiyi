da769be225ce6182a317c74fcd7c8e5177f487d1
suiyi-light <songyulong@suiyil.cn>
2020-08-27 15:16:52 +0800

    upload

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// array of Monitor-specific API keys or Read-Only API keys to list all monitors
+var __apiKeys = [
+	'm785621193-5b5b5749768bac28e073c97c', //index
+	'm785621187-7352e01a7d3c87bc9f910807', //Blog
+	'm785621198-7ab5a966925ad7607882f0a3', //upuss
+	'm785621197-dac34036e98bf16b4322a305', //t
+                'm785621425-dea4d9234651279d2ce82726',//mail
+                'm785621461-ffd6f5b8daffc3076350c8f5',//pan
+                'm785621427-8bfb95eddfd036327a2e729d',//status
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
